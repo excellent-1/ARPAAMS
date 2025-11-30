a4a5015d6c04d1d05fb11ac05470008351db9191
excellent-1 <excellentindevelopment@gmail.com>
2025-11-30 00:38:00 -0600

    Initial commit of C++ ARPAAMS Project

---
+++ _include/IBeamformingStrategy.h
@@ -0,0 +1,13 @@
+#ifndef IBEAMFORMINGSTRATEGY_H
+#define IBEAMFORMINGSTRATEGY_H
+// Forward declarations or actual definitions for AntennaArray and TargetInfo
+class AntennaArray;
+class TargetInfo;
+
+class IBeamformingStrategy 
+{
+public:
+    virtual void applyBeamforming(AntennaArray& array, const TargetInfo& target) = 0;
+    virtual ~IBeamformingStrategy() = default;
+};
+#endif // IBEAMFORMINGSTRATEGY_H
